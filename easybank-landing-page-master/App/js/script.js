868649155a38c958cffbee8a997657ecfd43fd8d
konpeeyush <piyushksingh2589@gmail.com>
2022-08-18 11:24:25 +0530

    Adding some changes.

--- easybank-landing-page-master/App/js/script.js
+++ easybank-landing-page-master/App/js/script.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+const btnHamburger=document.querySelector('#btnHamburger') ;
+const header=document.querySelector('header') ;
+const overlay=document.querySelector('.overlay') ;
+btnHamburger.addEventListener('click', () => {
+
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }else{
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }  
+}
+);
