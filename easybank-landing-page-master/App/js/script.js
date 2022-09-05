20d9356b4ac025dd44b8ee94d46c732422ef49a4
konpeeyush <piyushksingh2589@gmail.com>
2022-09-05 17:08:37 +0530

    Updated

--- easybank-landing-page-master/App/js/script.js
+++ easybank-landing-page-master/App/js/script.js
@@ -1,16 +1,31 @@
-const btnHamburger=document.querySelector('#btnHamburger') ;
-const header=document.querySelector('header') ;
-const overlay=document.querySelector('.overlay') ;
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
 btnHamburger.addEventListener('click', () => {
 
-    if(header.classList.contains('open')){
+    if (header.classList.contains('open')) {
+        //close menu
+        body.classList.remove('noscroll');
         header.classList.remove('open');
         overlay.classList.remove('fade-in');
         overlay.classList.add('fade-out');
-    }else{
+        fadeElems.forEach(elem => {
+            elem.classList.remove('fade-in');
+            elem.classList.add('fade-out');
+        })
+    }
+    else {
+        //open menu
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
-    }  
+        fadeElems.forEach(function (elem) {
+            elem.classList.remove('fade-out');
+            elem.classList.add('fade-in');
+        });
+
+    }
 }
 );
